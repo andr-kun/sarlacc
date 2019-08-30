259e43adf622b7e327dc933f7bbb2cfe6570712e
Andrian <ayang@ebi.ac.uk>
2019-08-30 16:51:27 +0100

    Modified barcodeAlign to perform comparison of all barcodes within each task

--- R/barcodeAlign.R
+++ R/barcodeAlign.R
@@ -17,31 +17,38 @@ barcodeAlign <- function(sequences, barcodes, gapOpening=5, gapExtension=1, BPPA
         on.exit(bpstop(BPPARAM), add=TRUE)
     }
 
-    for (b in seq_along(barcodes)) {
-        current <- toupper(as.character(barcodes[b]))
-        scores <- bpmapply(FUN=.align_BA_internal, barcodes=by.core, 
-            MoreArgs=list(reference=barcodes[b], gap.opening=gapOpening, gap.extension=gapExtension),
-            BPPARAM=BPPARAM, SIMPLIFY=FALSE, USE.NAMES=FALSE)
-        scores <- unlist(scores)
-
-        # Need to update both the best and the next best on record.
-        keep <- scores > current.score
-        second.keep <- !keep & scores > next.best
-
-        current.id[keep] <- b
-        next.best[keep] <- current.score[keep]
-        current.score[keep] <- scores[keep]
-        next.best[second.keep] <- scores[second.keep]
-    }
-
-    output <- DataFrame(barcode=current.id, score=current.score, gap=current.score - next.best)
+    outputs <- bpmapply(FUN=.align_BA_internal, barcodes=by.core, 
+        MoreArgs=list(reference=barcodes, gap.opening=gapOpening, gap.extension=gapExtension),
+        BPPARAM=BPPARAM, SIMPLIFY=FALSE, USE.NAMES=FALSE)
+    
+    output <- Reduce(rbind, outputs)
     metadata(output) <- list(gapOpening=gapOpening, gapExtension=gapExtension, barcodes=barcodes)
     output
 }
 
 #' @importFrom Biostrings quality
-.align_BA_internal <- function(barcodes, reference, gap.opening, gap.extension) {
+.align_BA_internal <- function(barcodes, references, gap.opening, gap.extension) {
     quals <- quality(barcodes)
-    .Call(cxx_barcode_align, barcodes, quals, .create_encoding_vector(quals), 
-            gap.opening, gap.extension, reference)
+    
+    current.score <- next.best <- rep(-Inf, length(barcodes))
+    current.id <- rep(NA_integer_, length(barcodes))
+    
+    for (r in seq_along(references)) {
+        reference <- toupper(as.character(references[r]))
+        scores <- .Call(cxx_barcode_align, barcodes, quals, .create_encoding_vector(quals), 
+                    gap.opening, gap.extension, reference)
+        
+        scores <- unlist(scores)
+        
+        # Need to update both the best and the next best on record.
+        keep <- scores > current.score
+        second.keep <- !keep & scores > next.best
+        
+        current.id[keep] <- r
+        next.best[keep] <- current.score[keep]
+        current.score[keep] <- scores[keep]
+        next.best[second.keep] <- scores[second.keep]
+    }
+    
+    DataFrame(barcode=current.id, score=current.score, gap=current.score - next.best)
 }
